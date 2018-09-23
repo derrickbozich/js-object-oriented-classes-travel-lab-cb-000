21747260c85fb3e3dff9204409692b52abbb43e2
derrick b <dbozich1@gmail.com>
2018-09-23 04:04:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,10 @@ class Route {
     this.endingLocation = endingLocation;
   }
 
-  let eastWest = ['1st Avenue','2nd Avenue', '3rd Avenue','Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
 
   blocksTravelled(){
+    let eastWest = ['1st Avenue','2nd Avenue', '3rd Avenue','Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+
     let vertical;
     let horizontal;
 
