4edca8859f9c379d725da1a03ed3835407e4b91c
derrick b <dbozich1@gmail.com>
2018-09-23 03:17:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ class Driver {
     this.startDate = new Date(98,1);
   }
   yearsExperienceFromBeginningOf(year){
-    dateYear = new Date(`${year}`)
+    dateYear = new Date(`${year}`);
 
-    return year - this.startDate.getFullYear();
+    return dateYear - this.startDate.getFullYear();
   }
 }
