b1bff294935f759c2b35f60e9acc85f70b29f0f7
derrick b <dbozich1@gmail.com>
2018-09-23 03:28:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ class Driver {
   yearsExperienceFromBeginningOf(year){
     dateYear = new Date(`${year}`);
     gap = parseInt((dateYear - this.startDate.getFullYear())/(365*24*60*60*1000));
-    debugger
+    return gap;
   }
 }
