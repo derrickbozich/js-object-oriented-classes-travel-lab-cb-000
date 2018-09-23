cab2a6ac80a568423097747ac02496b7cf893d72
derrick b <dbozich1@gmail.com>
2018-09-23 03:39:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ class Driver {
     this.name = name;
     this.startDate = new Date(startDate);
   }
+
   yearsExperienceFromBeginningOf(year){
     let dateYear = new Date(`${year}`);
     let gap = parseInt((dateYear - this.startDate)/(365*24*60*60*1000));
     return gap;
   }
+}
 
+class Route {
+  constructor(beginningLocation, endingLocation)
 }
